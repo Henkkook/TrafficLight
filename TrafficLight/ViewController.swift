bb433072442e10478e27c412c28b5305312f4451
D <K>
2022-10-23 18:20:50 +0500

    My strange home work

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var viewRedColor: UIView!
+    
+    @IBOutlet var viewYellowColor: UIView!
+    
+    @IBOutlet var viewGreenColor: UIView!
+    
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+       
     }
 
-
+    @IBAction func startButtonTapped() {
+    }
+    
 }
 
